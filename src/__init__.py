9c13853b5be0aaab49030ada928869f2d246af6c
buweishi <wephiles@163.com>
2024-08-22 17:32:03 +0800

    Ready to code 2024.08.22

---
+++ src/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @File     :__init__.py.py
+# @Author   :Admin@20866
+# @Project  :black_epic
+# @Time     :2024/8/22 17:31:00
+# @IDE      :PyCharm
+# @Describe :this is the description.
+# @Motto    :You must find your place in the circle of life.
+# @WebSite  :https://github.com/search?q=wephiles
